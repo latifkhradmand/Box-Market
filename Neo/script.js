c38562ae08f87e1e9955d79528e9f74172e858eb
Rohullah Ahmadi <rohullahahmadi531@gmail.com>
2026-02-28 21:03:17 +0430

    script added

--- Neo/script.js
+++ Neo/script.js
@@ -1,7 +1,20 @@
 const menubtn = document.querySelectorAll(".mobileMenu button");
-const menubtntext = document.querySelectorAll(".mobileMenu button span");
 menubtn.forEach((btn) => {
   btn.addEventListener("click", () => {
-    btn.classList.toggle("mobileMenuActive");
+    menubtn.forEach((btn) => {
+      btn.classList.remove("mobileMenuActive");
+    });
+    btn.classList.add("mobileMenuActive");
   });
+});
+
+const burgerfullmenu = document.querySelector(".burgerfullmnu");
+const burgerBtn = document.querySelector(".burgerBtn");
+const burgerBtnicon = document.querySelector(".burgerBtn i");
+
+burgerBtn.addEventListener("click", () => {
+  burgerfullmenu.classList.toggle("translate-x-60");
+  burgerBtn.classList.toggle("-rotate-[90deg]");
+  burgerBtnicon.classList.toggle("fa-bars");
+  burgerBtnicon.classList.toggle("fa-close");
 });
